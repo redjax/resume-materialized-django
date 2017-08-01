fb76eb1abf468a4746a177f5f972c34257d6176c
Jack Kenyon <jackenyon@gmail.com>
2017-08-01 12:40:51 -0400

    Init

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jack/Documents/github/python/resume-materialized-django/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
